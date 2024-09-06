6a394af2f74814c233fb5bc7c27509a94ef1c85c
Cole Morgan <colemmorgann@gmail.com>
2024-09-06 19:54:20 -0400

    Changed animation behavior for better performance

--- src/components/motion-components/AnimatedFadeUp.tsx
+++ src/components/motion-components/AnimatedFadeUp.tsx
@@ -14,8 +14,8 @@ const AnimatedFadeUp: React.FC<AnimatedFadeUpProps> = ({ children, threshold }) 
   return (
     <motion.div
       ref={ref}
-      initial={{ opacity: 0, y: 10 }}
-      animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 10 }}
+      initial={{ opacity: 0 }}
+      animate={isInView ? { opacity: 1 } : { opacity: 0 }}
       transition={{ duration: 0.4, ease: "easeInOut" }}
       className="gpu-accelerated"
     >
